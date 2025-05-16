ad229c22c1bf81809edd67622a8ea7f38d083748
Isabelly Santos <isabellyfelixsantos12@gmail.com>
2025-05-16 20:55:33 -0300

    Add files via upload

---
+++ js/ex2.js
@@ -0,0 +1,19 @@
+/*
+Instruções:
+Crie um HTML básico e inclua o link externo para o arquivo script.js
+
+No JavaScript:
+Crie uma variável com a idade de uma pessoa. Verifique com um operador se ela é maior ou igual a 18 anos.
+
+Mostre no console.log() uma mensagem assim:
+console.log("É maior de idade?", maiorDeIdade);
+
+Use:
+let
+console.log()
+
+*/
+let idade = 20;
+let maiorDeIdade = idade >= 18;
+
+console.log("É maior de idade?", maiorDeIdade);
